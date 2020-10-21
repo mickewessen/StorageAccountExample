7350a63bd4ae1eb083f38268cc41ed3d3e6d4ae2
Micke Wessén <micke.wessen@hotmail.com>
2020-10-21 11:32:25 +0200

    Add project files.

---
+++ StorageAccount/Program.cs
@@ -0,0 +1,38 @@
+﻿using StorageAccount.Services;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StorageAccount
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=mickewessenstorageacc;AccountKey=+Ggh1rnDgltapD8FWlbbeh7pcT/VphkQAPmpn9uLOoNeg629a8+x2Ly05TlgJqon4N0YmcQbm1LopaPxABo5aA==;EndpointSuffix=core.windows.net";
+            var containerName = "mickewessenstorageacc";
+
+            var fileName = $"myfile-{Guid.NewGuid()}.txt";
+            var content = "This is the content of the file";
+
+            var filePath = Path.Combine(@"C:\Users\Micke\Desktop\EC-WIN20\Files\mickewessenstorageacc\", fileName);
+            var downloadPath = Path.Combine(@"C:\Users\Micke\Desktop\EC-WIN20\Files\mickewessenstorageacc\downloads", fileName);
+
+            Console.WriteLine("Initialize Storage Account with containerName: " + containerName);
+            await StorageService.InitializeStorageAsync(connectionString, containerName);
+
+            Console.WriteLine("Creating and writing content in file: " + filePath);
+            await StorageService.WriteToFileAsync(filePath, content);
+
+            Console.WriteLine("Uploading file to Azure Storage Blob in container: " + containerName);
+            await StorageService.UploadFileAsync(filePath);
+
+            Console.WriteLine("Downloading file from Azure Storage Blob to: " + Path.GetDirectoryName(downloadPath));
+            await StorageService.DownloadFileAsync(downloadPath);
+
+            Console.WriteLine("Reading content from file: " + downloadPath);
+            Console.WriteLine(await StorageService.ReadDownloadedFileAsync(downloadPath));
+        }
+    }
+}
---
+++ StorageAccount/Services/StorageService.cs
@@ -0,0 +1,107 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StorageAccount.Services
+{
+    public static class StorageService
+    {
+        private static BlobContainerClient _containerClient { get; set; }
+        private static BlobClient _blobClient { get; set; }
+
+
+        public static async Task InitializeStorageAsync(string connectionString, string containerName, bool uniqueName = false)
+        {
+            if (uniqueName)
+                containerName = $"{containerName}-{Guid.NewGuid()}";
+
+            try
+            {
+                BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+                try
+                {
+                    _containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+                }
+                catch 
+                {
+                    try
+                    {
+                        _containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+                    }
+                    catch { }
+                }
+            }
+            catch { }
+
+        }
+
+
+        public static async Task WriteToFileAsync(string @filePath, string content)
+        {
+
+            try
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+
+                await File.WriteAllTextAsync(filePath, content);
+            }
+            catch { }
+
+        }
+
+
+        public static async Task UploadFileAsync(string @filePath)
+        {
+            try
+            {
+                _blobClient = _containerClient.GetBlobClient(Path.GetFileName(filePath));
+
+                using FileStream fileStream = File.OpenRead(filePath);
+                await _blobClient.UploadAsync(fileStream, true);
+                fileStream.Close();
+
+                File.Delete(filePath);
+            }
+            catch { }
+
+        }
+
+
+        public static async Task DownloadFileAsync(string @downloadPath)
+        {
+            try
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(downloadPath)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(downloadPath));
+
+                BlobDownloadInfo download = await _blobClient.DownloadAsync();
+
+                using FileStream fileStream = File.OpenWrite(downloadPath);
+                await download.Content.CopyToAsync(fileStream);
+                fileStream.Close();
+
+            }
+            catch { }
+            Console.WriteLine(await File.ReadAllTextAsync(downloadPath));
+        }
+
+        public static async Task<string> ReadDownloadedFileAsync(string @downloadPath)
+        {
+            try
+            {
+                return await File.ReadAllTextAsync(downloadPath);
+            }
+            catch { }
+
+            return "";
+        }
+
+    }
+}
